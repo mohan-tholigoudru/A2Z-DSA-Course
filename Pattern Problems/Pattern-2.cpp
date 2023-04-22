470e1987909260a29b70a838a65f7d2e7ebb265b
Mohan TG <mohantg183@gmail.com>
2023-04-22 18:07:40 +0530

    pattern-2 solved.

--- Pattern Problems/Pattern-2.cpp
+++ Pattern Problems/Pattern-2.cpp
@@ -1,23 +1,24 @@
 /*
-Rectangular Star Pattern
+Right-angled triangle Pattern
 
 Problem Statement: Given an integer N, print the following pattern.
-* * * * *
-* * * * *
-* * * * *
-* * * * *
+*
+* *
+* * *
+* * * *
 * * * * *
 
 */
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
     int n;
     cin >> n;
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
             cout << "* ";
         }
         cout << endl;
